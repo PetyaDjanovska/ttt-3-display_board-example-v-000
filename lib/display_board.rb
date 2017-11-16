829a280858dcb8be912cf57383b3589a097563bb
Petya Djanovska <petya.djanovska@gmail.com>
2017-11-16 18:24:56 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,9 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
   puts "   |   |   "
-  puts "_ _ _ _ _ _ _ _ _ _ _"
+  puts "___________"
   puts "   |   |   "
-  puts "_ _ _ _ _ _ _ _ _ _ _"
+  puts "___________"
   puts "   |   |   "
 end
 
